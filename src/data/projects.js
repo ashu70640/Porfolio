78f582858a5462abb0b91ba4d0cdda109ad14225
ashu70640 <ashu70640@gmail.com>
2026-02-05 18:52:02 +0530

    added cinespark project link

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,31 +4,37 @@
  */
 export const projects = [
   {
-  id: '1',
-  title: 'Irecon – Billing & Tenant Management Platform',
-  description:
-    'Enterprise billing platform developed for internal teams, featuring invoice generation, tax and discount calculations, and usage dashboards.',
-  techStack: ['React.js', 'JavaScript', 'Node.js', 'Express.js', 'MongoDB', 'Chart.js'],
-  githubUrl: '', // Internal project – keep empty or remove button
-  liveUrl: '',
-},
-{
-  id: '2',
-  title: 'JobSphere – Job Tracking Application',
-  description:
-    'Full-stack job tracking application with authentication, role-based access control, and advanced filtering for managing job applications.',
-  techStack: ['Node.js', 'Express.js', 'MongoDB', 'JWT', 'REST APIs'],
-  githubUrl: 'https://github.com/<your-username>/jobsphere',
-  liveUrl: '',
-},
-{
-  id: '3',
-  title: 'CineSpark – Movie Trailer & Info App',
-  description:
-    'Responsive movie discovery platform with trailer search, ratings-based filtering, and conversational search using the OpenAI API',
-  techStack: ['React.js', 'Redux', 'Firebase', 'TMDB API','OpenAI API'],
-  githubUrl: 'https://github.com/<your-username>/cinespark',
-  liveUrl: '',
-}
-
+    id: "1",
+    title: "Irecon – Billing & Tenant Management Platform",
+    description:
+      "Enterprise billing platform developed for internal teams, featuring invoice generation, tax and discount calculations, and usage dashboards.",
+    techStack: [
+      "React.js",
+      "JavaScript",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "Chart.js",
+    ],
+    githubUrl: "", // Internal project – keep empty or remove button
+    liveUrl: "",
+  },
+  {
+    id: "2",
+    title: "JobSphere – Job Tracking Application",
+    description:
+      "Full-stack job tracking application with authentication, role-based access control, and advanced filtering for managing job applications.",
+    techStack: ["Node.js", "Express.js", "MongoDB", "JWT", "REST APIs"],
+    githubUrl: "https://github.com/<your-username>/jobsphere",
+    liveUrl: "",
+  },
+  {
+    id: "3",
+    title: "CineSpark – Movie Trailer & Info App",
+    description:
+      "Responsive movie discovery platform with trailer search, ratings-based filtering, and conversational search using the OpenAI API",
+    techStack: ["React.js", "Redux", "Firebase", "TMDB API", "OpenAI API"],
+    githubUrl: "https://github.com/ashu70640/CineSpark",
+    liveUrl: "https://cinespark-7dhm.onrender.com",
+  },
 ];
